35004cc5b9e5ade2d89c5cdffb26f191268d30ce
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-08-06 16:49:31 +0800

    check user is logged in on Dasboard controller

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -3,6 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
+	
+	public function __construct()
+	{
+		parent::__construct();
+
+		// check if user is logged in
+
+		if (!$this->ion_auth->logged_in())
+		{
+			redirect('auth/login');
+		}
+	}
+
+
 	public function index()
 	{
 		
