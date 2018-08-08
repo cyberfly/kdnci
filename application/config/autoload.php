cca28aa4f36c4fe761716cedfc650cd08f0dd527
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-08-08 11:20:33 +0800

    autoload form helper and form validation

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array(APPPATH.'third_party/ion_auth');
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('ion_auth','database');
+$autoload['libraries'] = array('ion_auth','database','form_validation');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','user');
+$autoload['helper'] = array('url','user','form');
 
 /*
 | -------------------------------------------------------------------
