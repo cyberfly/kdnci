680d1f9e87ebb0150352cf066a067a345f8cae94
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-08-08 11:19:28 +0800

    check user login for Ticket controller

--- application/controllers/Ticket.php
+++ application/controllers/Ticket.php
@@ -4,6 +4,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Ticket extends CI_Controller {
 
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		// check if user is logged in
+
+		if (!$this->ion_auth->logged_in())
+		{
+			redirect('auth/login');
+		}
+
+		$this->load->model('category_model');
+	}
+
+
 	// senarai tiket
 
 	public function index()
