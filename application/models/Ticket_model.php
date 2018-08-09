16cc78763bd7c11747c5e09f7187def0cfdf7f80
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-08-09 08:32:14 +0800

    new getUserTickets function, refactor getAll() to prevent repetitive code with getUserTickets

--- application/models/Ticket_model.php
+++ application/models/Ticket_model.php
@@ -6,6 +6,28 @@ class Ticket_model extends CI_Model {
 
 	public function getAll()
 	{
+		$this->queryTicket();
+		
+		$query = $this->db->get();
+
+		// $query = $this->db->get('tickets');
+
+		return $query->result();
+	}
+
+	public function getUserTickets($user_id)
+	{
+		$this->queryTicket();
+
+		$this->db->where('user_id', $user_id);
+
+		$query = $this->db->get();
+
+		return $query->result();
+	}
+
+	private function queryTicket()
+	{
 		$this->db->select('tickets.id, tickets.subject, tickets.description, tickets.status_id, tickets.category_id, tickets.created_at, categories.title as category_title, statuses.title as status_title');
 
 		$this->db->from('tickets');
@@ -15,12 +37,6 @@ class Ticket_model extends CI_Model {
 		$this->db->join('categories', 'categories.id = tickets.category_id', 'left');
 
 		$this->db->join('statuses', 'statuses.id = tickets.status_id', 'left');
-
-		$query = $this->db->get();
-
-		// $query = $this->db->get('tickets');
-
-		return $query->result();
 	}
 
 	public function insert($data)
