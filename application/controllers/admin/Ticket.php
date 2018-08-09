628e1e2ef79431a4f06f719e660b3ad4cad2f340
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-08-09 10:49:33 +0800

    new Admin Ticket controller

---
+++ application/controllers/admin/Ticket.php
@@ -0,0 +1,62 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+// Ni admin ticket, hati-hati
+
+class Ticket extends MY_Controller {
+
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('category_model');
+		$this->load->model('status_model');
+	}
+
+
+	// senarai tiket
+
+	public function index()
+	{
+		$data = array();
+
+		// dapatkan data dari table tickets
+
+		$tickets = $this->ticket_model->getAll();
+
+		// pass tickets data to view
+
+		$data['tickets'] = $tickets;
+
+		// load index.php from folder tickets as content
+
+		$data['content'] = 'admin/tickets/index';
+
+		$this->load->view('templates/backend', $data);
+	}
+
+	// edit ticket form
+
+	public function edit()
+	{
+
+	}
+
+	// show ticket details
+
+	public function show()
+	{
+
+	}
+
+	// delete ticket
+
+	public function delete()
+	{
+
+	}
+
+}
