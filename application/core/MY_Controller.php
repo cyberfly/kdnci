171d12465ad92a9e704ebe1850d8fbdb51280df1
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-08-09 10:04:44 +0800

    check user is logged in MY_Controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -7,6 +7,14 @@ class MY_Controller extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
+
+
+		// check if user is logged in
+
+		if (!$this->ion_auth->logged_in())
+		{
+			redirect('auth/login');
+		}
 	}
 
 }
