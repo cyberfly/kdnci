056559e07a4afa0849643af54faaddfdf0726644
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-08-12 13:53:52 +0800

    new dashboard page to show chart

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,20 @@
+<div class="page-header">
+  <h1 class="page-title">
+    Ticket Report
+  </h1>
+</div>
+
+<div class="row">
+  <div class="col-lg-6">
+
+	<canvas id="myChart" width="400" height="400"></canvas>
+
+  </div>
+
+</div>
+
+
+
+
+
+
