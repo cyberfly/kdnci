f905e7936b209c17633ad07cfa2a503df86fb403
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2018-08-12 13:55:06 +0800

    update Dashboard controller index function to prepare chart data, pass it into dashboard/index view

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -12,13 +12,46 @@ class Dashboard extends MY_Controller {
 
 	public function index()
 	{
-		
 		// declare empty array
 		$data = array();
 
-		// content page file name
+		$ticket_status_count = $this->ticket_model->ticketsStatusCount();
 
-		$data['content'] = 'democontent';	
+		$label_data = array();
+		$count_data = array();
+		$background_data = array();
+
+		foreach ($ticket_status_count as $status_count) {
+
+			// set label data
+			$label_data[] = $status_count->title;
+
+			// set count data
+
+			$count_data[] = $status_count->status_count;
+				
+		}
+
+		// encode to json before pass to js
+
+
+		$label_data = json_encode($label_data);
+		$count_data = json_encode($count_data);
+		$background_data = json_encode($background_data);
+
+		// passkan JS data
+
+		$data['label_data'] = $label_data;	
+		$data['count_data'] = $count_data;		
+		$data['background_data'] = $background_data;		
+
+		// load page js files
+
+		$data['page_js'] = array('dashboard/index-js');
+
+		// set content page to show
+
+		$data['content'] = 'dashboard/index';	
 
 		$this->load->view('templates/backend', $data);
 	}
